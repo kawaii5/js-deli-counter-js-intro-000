7bbdf36de9f00f950967516fe4a1e0fb19db950e
Michelle Wang <oc3anazns3a@gmail.com>
2020-12-29 01:04:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function currentLine(line) {
   let str = "The line is currently: ";
   let len = line.length;
   if(len == 0) {
-    str.replace(": ", " empty.");
+    str = str.replace(": ", " empty.");
     return str;
   }
   for(let i = 0; i < len; ++i) {
