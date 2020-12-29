c5a149fb18641881d728619139fd7f4667f696ae
Michelle Wang <oc3anazns3a@gmail.com>
2020-12-29 00:56:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ function takeANumber(line, person) {
   line.push(person);
   return "Welcome, " + person + ". You are number " + line.length + " in line.";
 }
+
+function nowServing(line) {
+  return line.shift();
+}
